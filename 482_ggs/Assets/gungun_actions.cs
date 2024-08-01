4d420a32cb7fca158a965e8dc0ab5298f8a34882
hazellefacil <59992832+hazellefacil@users.noreply.github.com>
2024-08-01 13:30:00 -0700

    feat: socket set up

--- 482_ggs/Assets/gungun_actions.cs
+++ 482_ggs/Assets/gungun_actions.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Net.Sockets;
+using System.Text;
 
 public class gungun_actions : MonoBehaviour
 {
@@ -9,12 +12,32 @@ public class gungun_actions : MonoBehaviour
     private int currentAction; 
     private string[] actions = {"block","reload","shoot"};
     private string idleAnimation = "idle";
+    TcpClient client;
+    NetworkStream stream;
 
     void Start()
     {
+        client = new TcpClient("localhost", 65432);
+        stream = client.GetStream();
         mAnimator = GetComponent<Animator>();
         mAnimator.Play(idleAnimation); 
-        Invoke(nameof(UpdateAction), 3f);
+        Invoke(nameof(UpdateAction), 1f);
+    }
+
+    void Update()
+    {
+        if (stream.DataAvailable)
+        {
+            byte[] data = new byte[256];
+            int bytesRead = stream.Read(data, 0, data.Length);
+            string gesture = Encoding.ASCII.GetString(data, 0, bytesRead).Trim();
+            ProcessGesture(gesture);
+        }
+    }
+
+    void ProcessGesture(string gesture)
+    {
+        Debug.Log(gesture);
     }
 
     void UpdateAction()
@@ -34,7 +57,7 @@ public class gungun_actions : MonoBehaviour
     void ReturnToIdle()
     {
         mAnimator.Play(idleAnimation); 
-        Invoke(nameof(UpdateAction), 3f);
+        Invoke(nameof(UpdateAction), 1f);
     }
 
 }
